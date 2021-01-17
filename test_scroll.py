f6b1b893b85ad6435a0c2997530f4ca67487a2e4
Arendelle12 <gabriela.bartoszewska@student.put.poznan.pl>
2021-01-17 15:34:34 +0100

    IDK

--- test_scroll.py
+++ test_scroll.py
@@ -11,63 +11,57 @@ class MyApp(tk.Tk):
 
         self.title(title)
         self.configure(background="Gray")
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
+        # self.columnconfigure(0, weight=1)
+        # self.rowconfigure(0, weight=1)
         #self.geometry('800x800')
 
-        master_frame = tk.Frame(self, bg="Light Green", bd=3, relief=tk.RIDGE)
-        master_frame.grid(sticky=tk.NSEW)
-        master_frame.columnconfigure(0, weight=1)
-        master_frame.rowconfigure(1, weight=1)
+        master_frame = tk.Frame(self, bd=3, relief=tk.RIDGE)
+        master_frame.grid(row=0, column=0)
+        #sticky=tk.NSEW)
+        # master_frame.columnconfigure(0, weight=1)
+        # master_frame.rowconfigure(1, weight=1)
 
 
         top_frame = tk.Frame(master_frame, bg="Light Blue", bd=3, relief=tk.RIDGE)
-        top_frame.grid(sticky=tk.NSEW)
-        top_frame.columnconfigure((0,1,2), weight=1)
+        top_frame.grid(row=0, column=0)
+        # top_frame.grid(sticky=tk.NSEW)
+        # top_frame.columnconfigure((0,1,2), weight=1)
 
         
 
-        frame1a = tk.Frame(top_frame, bg="Green", bd=2, relief=tk.GROOVE)
-        frame1a.grid(row=0, column=0, sticky=tk.NSEW)
-        frame1a.columnconfigure(0, weight=1)
-        #frame1.rowconfigure((0,1), weight=1)
+        frame1a = tk.Frame(top_frame, bg="Light blue", bd=2, relief=tk.GROOVE)
+        frame1a.grid(row=0, column=0)
+        #, sticky=tk.NSEW)
+        #frame1a.columnconfigure(0, weight=1)
 
-        frame1b = tk.Frame(top_frame, bg="Blue", bd=2, relief=tk.GROOVE)
-        frame1b.grid(row=0, column=1, sticky=tk.NSEW)
-        frame1b.columnconfigure(0, weight=1)
-        #frame1b.rowconfigure((0,1), weight=1)
+        frame1b = tk.Frame(top_frame, bg="Light blue", bd=2, relief=tk.GROOVE)
+        frame1b.grid(row=0, column=1)#, sticky=tk.NSEW)
+        #frame1b.columnconfigure(0, weight=1)
 
-        frame1c = tk.Frame(top_frame, bg="Red", bd=2, relief=tk.GROOVE)
-        frame1c.grid(row=0, column=2, sticky=tk.NSEW)
-        frame1c.columnconfigure(0, weight=1)
-        #frame1c.rowconfigure((0,1), weight=1)
+        frame1c = tk.Frame(top_frame, bg="Light blue", bd=2, relief=tk.GROOVE)
+        frame1c.grid(row=0, column=2)#, sticky=tk.NSEW)
+        #frame1c.columnconfigure(0, weight=1)
 
-        label1a = tk.Label(frame1a, text="Frame1 Contents", bg=LABEL_BG)
-        label1a.grid(row=0, column=0, pady=5, sticky=tk.N)
-        #label1.columnconfigure(0, weight=1)
+        label1a = tk.Label(frame1a, text="Frame1 Contents", bg="Light blue", width=20)
+        label1a.grid(row=0, column=0, padx=3, pady=5)#, sticky=tk.N)
 
-        label1b = tk.Label(frame1b, text="Frame1b Contents", bg=LABEL_BG)
-        label1b.grid(row=0, column=0, pady=5, sticky=tk.N)#tk.NSEW)
-        #label1b.columnconfigure(0, weight=1)
+        label1b = tk.Label(frame1b, text="Frame1b Contents", bg="Light blue", width=20)
+        label1b.grid(row=0, column=0, padx=3, pady=5)#, sticky=tk.N)
 
-        label1c = tk.Label(frame1c, text="Frame1c Contents", bg=LABEL_BG)
-        label1c.grid(row=0, column=0, pady=5, sticky=tk.N)
-        #label1c.columnconfigure(0, weight=1)
+        label1c = tk.Label(frame1c, text="Frame1c Contents", bg="Light blue", width=20)
+        label1c.grid(row=0, column=0, padx=3, pady=5)#, sticky=tk.N)
 
         cb_var1a = tk.IntVar()
-        checkbutton1a = tk.Checkbutton(frame1a, text="StartCheckBox", variable=cb_var1a)
-        checkbutton1a.grid(row=1, column=0, padx=2, sticky=tk.N)
-        # checkbutton1.columnconfigure(0, weight=1)
+        checkbutton1a = tk.Checkbutton(frame1a, text="StartCheckBox", variable=cb_var1a, bg="Light blue")
+        checkbutton1a.grid(row=1, column=0, padx=2)#, sticky=tk.N)
 
         cb_var1b = tk.IntVar()
-        checkbutton1b = tk.Checkbutton(frame1b, text="Startbb", variable=cb_var1b)
-        checkbutton1b.grid(row=1, column=0, padx=2, sticky=tk.N)
-        # checkbutton1b.columnconfigure(0, weight=1)
+        checkbutton1b = tk.Checkbutton(frame1b, text="Startbb", variable=cb_var1b, bg="Light blue")
+        checkbutton1b.grid(row=1, column=0, padx=2)#, sticky=tk.N)
 
         cb_var1c = tk.IntVar()
-        checkbutton1c = tk.Checkbutton(frame1c, text="Startccc", variable=cb_var1c)
-        checkbutton1c.grid(row=1, column=0, padx=2, sticky=tk.N)
-        # checkbutton1c.columnconfigure(0, weight=1)
+        checkbutton1c = tk.Checkbutton(frame1c, text="Startccc", variable=cb_var1c, bg="Light blue")
+        checkbutton1c.grid(row=1, column=0, padx=2)#, sticky=tk.N)
 
         #####
 
@@ -75,19 +69,15 @@ class MyApp(tk.Tk):
 
         # Create a frame for the canvas and scrollbar(s).
         frame2 = tk.Frame(master_frame)
-        frame2.grid(row=1, column=0, columnspan=3, sticky=tk.NSEW)
-        frame2.columnconfigure(0, weight=1)
-        frame2.rowconfigure(0, weight=1)
-
-        # label2 = tk.Label(frame2, text="Frame2 Contents", bg=LABEL_BG)
-        # label2.grid(row=0, column=0, pady=5, sticky=tk.NW)
+        frame2.grid(row=1, column=0, columnspan=3)#, sticky=tk.NSEW)
+        # frame2.columnconfigure(0, weight=1)
+        # frame2.rowconfigure(0, weight=1)
 
         # Add a canvas in that frame.
-        #Ten canvas dziala i jest responsive
-        canvas = tk.Canvas(frame2, bg="Yellow")
-        canvas.grid(row=0, column=0, sticky=tk.NSEW)
-        canvas.columnconfigure(0, weight=1)
-        canvas.rowconfigure(0, weight=1)
+        canvas = tk.Canvas(frame2)
+        canvas.grid(row=0, column=0)#, sticky=tk.NSEW)
+        # canvas.columnconfigure(0, weight=1)
+        # canvas.rowconfigure(0, weight=1)
 
         # Create a vertical scrollbar linked to the canvas.
         # to tez dziala
@@ -95,26 +85,28 @@ class MyApp(tk.Tk):
         vsbar.grid(row=0, column=1, sticky=tk.NS)
         canvas.configure(yscrollcommand=vsbar.set)
 
-        # # Create a horizontal scrollbar linked to the canvas.
-        # hsbar = tk.Scrollbar(frame2, orient=tk.HORIZONTAL, command=canvas.xview)
-        # hsbar.grid(row=1, column=0, sticky=tk.EW)
-        # canvas.configure(xscrollcommand=hsbar.set)
-
-        # Create a frame on the canvas to contain the buttons.
+                # Create a frame on the canvas to contain the buttons.
         ##################################
         # ten frame nie dziala, czerwone pole sie nie powieksza, moze to przez to, ze jest na canvas???
-        labels_frame = tk.Frame(canvas, bg="Red", bd=2)
-        labels_frame.grid(row=0, column=0, columnspan=3, sticky=tk.NSEW)
+        labels_frame = tk.Frame(canvas, bd=2)
+        labels_frame.grid(row=0, column=0, columnspan=3)#, sticky=tk.NSEW)
         # buttons_frame.columnconfigure(0, weight=1)
         #  
-        labels_frame.columnconfigure((0,1,2), weight=1)
+        # labels_frame.columnconfigure((0,1,2), weight=1)
         ################
 
         for i in range(15):
             for j in range(3):
-                label = tk.Label(master=labels_frame, text=f"Row {i} Column {j}")
-                label.grid(row=i, column=j, padx=5, pady=5, sticky=tk.NSEW)
-                label.columnconfigure(0, weight=1)
+                if j==0:
+                    label = tk.Label(master=labels_frame, text=f"Row {i} Column {j}", width=20)
+                    label.grid(row=i, column=j, padx=5, pady=5)#, sticky=tk.NSEW)
+                elif j==1:
+                    label = tk.Label(master=labels_frame, text=f"Row {i}", width=20)
+                    label.grid(row=i, column=j, padx=5, pady=5)#, sticky=tk.NSEW)
+                elif j==2:
+                    label = tk.Label(master=labels_frame, text=f"Row", width=20)
+                    label.grid(row=i, column=j, padx=5, pady=5)#, sticky=tk.NSEW)
+               # label.columnconfigure(0, weight=1)
 
         # for i in range(15):
         #     label = tk.Label(master=buttons_frame, text=f"Row {i} Column 0")
